7209b1bca224594d790a85f26131f8788377a73d
owlikesj <owlikesj@gmail.com>
2017-09-13 02:34:04 +0800

    Update basic expression and test code

--- js/basic-expression.js
+++ js/basic-expression.js
@@ -6,30 +6,43 @@ class BasicExpression {
   }
   validate () {
     const operator = this.operator
-    const needNum = BasicExpression.operandsNumOf(operator)
     let operands = this.operands
-    if (operator === '-' && operands.length === 1) {
-      operands.unshift(0)
+    if (operator && !isValidOperator(operator)) {
+      throw '"${operator}" is not a valid operator'
     }
-    if (needNum !== operands.length) {
-      throw `The ${operator} operation require ${needNum} operand${needNum > 1 ? 's' : ''}`
-    }
-    operands.forEach(operand => {
-      if (!BasicExpression.isValidOperand(operand)) {
-        throw `${JSON.stringify(operand)} is not a valid operand`
+    if (operator) {
+      const needNum = operandsNumOf(operator)
+      if (!needNum.includes(operands.length)) {
+        throw `The ${operator} operation require ${needNum.join(' or ')} operand${needNum[0] > 1 ? 's' : ''}`
       }
-    })
+      operands.forEach(operand => {
+        if (!isValidOperand(operand)) {
+          throw `${JSON.stringify(operand)} is not a valid operand`
+        }
+      })
+    } else {
+      if (!operands.length) {
+        throw `Empty operator require only one operand`
+      }
+      if (!isValidOperand(operands[0])) {
+        throw `${JSON.stringify(operands[0])} is not a valid operand`
+      }
+    }
+    
   }
   getResult () {
     let operands = this.operands
+    if (!this.operator) {
+      return operands[0]
+    }
     operands = operands.map(operand =>
             operand instanceof BasicExpression
             ? operand.getResult() : operand)
     switch (this.operator) {
       case '+':
-        return operands[0] + operands[1]
+        return operands[1] ? operands[0] + operands[1] : operands[0]
       case '-':
-        return operands[0] - operands[1]
+        return operands[1] ? operands[0] - operands[1] : -operands[0]
       case '×':
         return operands[0] * operands[1]
       case '÷':
@@ -38,20 +51,144 @@ class BasicExpression {
         return operands[0] / 100
     }
   }
-  static isValidOperand (operand) {
-    return typeof operand === 'number' || operand instanceof BasicExpression
-  }
-  static operandsNumOf (operator) {
-    switch (operator) {
-      case '%':
-        return 1
-      case '+':
-      case '-':
-      case '×':
-      case '÷':
-        return 2
-    }
+}
+
+function isValidOperand (operand) {
+  return typeof operand === 'number' || operand instanceof BasicExpression
+}
+function isValidOperator (operator) {
+  return ['+', '-', '×', '÷', '%'].includes(operator)
+}
+function precedence (operator) {
+  switch (operator) {
+    case '%':
+      return 1
+    case '+':
+    case '-':
+      return 2
+    case '×':
+    case '÷':
+      return 3
   }
 }
+function operandsNumOf (operator) {
+  switch (operator) {
+    case '%':
+      return [1]
+    case '+':
+    case '-':
+      return [2, 1]
+    case '×':
+    case '÷':
+      return [2]
+  }
+}
+function str2expr(str) {
+  const illegal = /[^\s\d\.\+\-×÷%\(\)]+|([\.×÷%]\s*){2,}|([\+\-]\s*){3,}/
+  const operRe = /[\+\-×÷%\(\)]|\d+\.?\d*|\.\d+/g
+  const opers = str.match(operRe)
+  if (illegal.test(str)) {
+    throw `"${str}" is not a valid expression`
+  }
+  let isParentheseOpen = 0
+  opers.forEach(oper => {
+    if (oper === '(') {
+      isParentheseOpen++
+    } else if (oper === ')') {
+      isParentheseOpen--
+    }
+    if (isParentheseOpen < 0) {
+      throw `"${str}" is not a valid expression`
+    }
+  })
+  if (isParentheseOpen) {
+    throw `"${str}" is not a valid expression`
+  }
+  return arr2expr(str.match(operRe))
+}
+function arr2expr(opers) {
+  // Remove outer parenthese
+  outer:
+  while (opers[0] === '(' && opers[opers.length - 1] === ')') {
+    let isParentheseOpen = 0
+    for (let i = 1; i < opers.length - 1; i++) {
+      if (opers[i] === '(') {
+        isParentheseOpen++
+      } else if (opers[i] === ')') {
+        isParentheseOpen--
+      }
+      if (isParentheseOpen < 0) {
+        break outer
+      }
+    }
+    opers.shift()
+    opers.pop()
+  }
+  if (opers.length === 1) {
+    return new BasicExpression('', parseFloat(opers[0]))
+  }
+  let left = opers
+  let right = []
+  let isParentheseOpen = 0
+  let item = ''
+  while (item = left.pop()) {
+    switch (item) {
+      case ')':
+        isParentheseOpen++
+        break
+      case '(':
+        isParentheseOpen--
+        break
+      case '+':
+      case '-':
+        if (isParentheseOpen || operandsNumOf(left[left.length - 1]) === 2) {
+          right.unshift(item)
+        } else {
+          return new BasicExpression(item,
+            left.length > 1 ? arr2expr(left) : parseFloat(left[0]),
+            right.length > 1 ? arr2expr(right) : parseFloat(right[0]))
+        }
+        break
+      default:
+        right.unshift(item)
+    }
+  }
+  isParentheseOpen = 0;
+  [left, right] = [right, left]
+  while (item = left.pop()) {
+    switch (item) {
+      case ')':
+        isParentheseOpen++
+        break
+      case '(':
+        isParentheseOpen--
+        break
+      case '×':
+      case '÷':
+        if (isParentheseOpen) {
+          right.unshift(item)
+        } else {
+          return new BasicExpression(item,
+            left.length > 1 ? arr2expr(left) : parseFloat(left[0]),
+            right.length > 1 ? arr2expr(right) : parseFloat(right[0]))
+        }
+        break
+      default:
+        right.unshift(item)
+    }
+  }
+  let first = right[0]
+  let last = right[right.length - 1]
+  if (first === '+' || first === '-') {
+    return new BasicExpression(right.shift(),
+      right.length > 1 ? arr2expr(right) : parseFloat(right[0]))
+  }
+  if (last === '%') {
+    return new BasicExpression(right.pop(),
+      right.length > 1 ? arr2expr(right) : parseFloat(right[0]))
+  }
+}
+
+BasicExpression.parseString = str2expr
 
 export default BasicExpression
--- test/basic-expression.js
+++ test/basic-expression.js
@@ -1,4 +1,4 @@
-import BasicExpression from '../js/basic'
+import BasicExpression from '../js/basic-expression'
 import assert from 'assert'
 
 function testAdd () {
@@ -8,4 +8,23 @@ function testAdd () {
   assert.equal(e2.getResult(), 6, `表达式${JSON.stringify(e1)}加3应该等于6`)
 }
 
+function testOne () {
+  let e1 = new BasicExpression('-', 5)
+  assert.equal(e1.getResult(), -5, '-5应该等于-5')
+  let e2 = new BasicExpression('%', 5)
+  assert.equal(e2.getResult(), 0.05, '5%应该等于0.05')
+}
+
+function testParse () {
+  let str = '1 + 2'
+  let e = new BasicExpression('+', 1, 2)
+  assert.deepEqual(BasicExpression.parseString(str), e, `${str}应该被解析成表达式对象${JSON.stringify(e)}`)
+  let longStr = '1+(5%+3)-.4×5÷6'
+  let result = 1+(5/100+3)-.4*5/6
+  assert.equal(BasicExpression.parseString(longStr).getResult(),
+    result, `${str}被解析成表达式对象计算结果得到${result}`)
+}
+
 testAdd()
+testOne()
+testParse()
